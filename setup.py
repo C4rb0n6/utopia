487d6942c3b1dbdcfee44b1cdc983bde7b1a8de6
charlie <c4rb0n6@gmail.com>
2024-03-20 01:21:14 -0500

    Fixed typo

--- setup.py
+++ setup.py
@@ -5,10 +5,12 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        'aiohttp==3.9.1',
+        'aiohttp==3.9.2',
         'discord==2.3.2',
         'python-dotenv==1.0.0',
-        'openai==1.8.0',
         'requests==2.31.0',
+        'google-ai-generativelanguage==0.4.0',
+        'google-generativeai==0.4.0'
+        'pillow==10.2.0',
     ],
 )
